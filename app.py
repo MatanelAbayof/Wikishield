8819f4529982af4d794aa744bb65928004166e2b
Matanel Abayof <MatanelAbayof@outlook.com>
2020-04-19 18:25:03 +0300

    test

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import toolforge
+import json
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hello!"
+    conn = toolforge.connect('enwiki_p')
+    dbs = conn.query("SHOW TABELS")
+    return json.dumps(dbs)
 
 # ------------------------------------------ start server --------------------------------------------
 if __name__ == '__main__':
