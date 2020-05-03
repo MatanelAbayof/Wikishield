0307440a68ff785c70f3969f70420a6b61494cf9
Matanel Abayof <MatanelAbayof@outlook.com>
2020-05-03 12:48:13 +0300

    db connection

--- db/wikishield_connection.py
+++ db/wikishield_connection.py
@@ -4,18 +4,23 @@ from db.db_connection import BaseDbConnection
 
 # ------------------------------------------------------------------------------------------------------------
 class DBConnection(BaseDbConnection):
+    _HOST = 'tools.db.svc.eqiad.wmflabs'
 
-    def __init__(self):
+    def __init__(self, sql_user_name: str='s54207'): #TODO: get user name from file
         """
         initialize connection to Wikishield database
         """
 
         super().__init__()
+        self.sql_user_name = sql_user_name
+
+    def _get_db_name(self):
+        return self.sql_user_name + '__wikishield_p'
 
     def _connect(self):
         """
         connect to DB
         """
 
-        args = { 'host': 'tools.db.svc.eqiad.wmflabs' }
-        self.ctx = toolforge.connect('s54207__wikishield_p', **args)
+        args = { 'host': self._HOST }
+        self.ctx = toolforge.connect(self._get_db_name(), **args) 
