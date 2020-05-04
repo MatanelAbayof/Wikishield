7781f1146155b5d54e57917091138b5e99ec0be6
Matanel Abayof <MatanelAbayof@outlook.com>
2020-05-04 16:29:51 +0300

    sync

--- tests/test.py
+++ tests/test.py
@@ -19,16 +19,33 @@ import os
 wd = os.path.expanduser("~/www/python/src")
 sys.path.append(wd)
 os.chdir(wd)
-from db.wikishield_connection import DBConnection as WS
+from db.wikimedia_connection import DBConnection as WD
 from db.connection_info import read_sql_user_name
 sql_user_name = read_sql_user_name()
-print('sql_user_name=', sql_user_name)
+from lang.langs import LangsManager, Lang
+lm = LangsManager()
+lang = lm.get_lang('en')
+wd = WD(sql_user_name, lang)
+wd._connect()
+from db.wikimedia_db import WikimediaDB
+wdb = WikimediaDB(wd.ctx, lang)
+revs = wdb.fetch_training_revs(10)
+print(revs)
+
+
+
+
+
+
+from db.wikishield_connection import DBConnection as WS
 ws = WS(sql_user_name)
 ws._connect()
-with ws.ctx.cursor() as cur:
-    cur.execute('SHOW TABLES')
-    rows = cur.fetchall()
-    print(rows)
+from db.wikishield_db import WikishieldDB
+wsb = WikishieldDB(ws.ctx, lang, sql_user_name)
+revs = wsb.fetch_training_set()
+print(revs)
+
+
 
 
 from lang.langs import LangsManager, Lang
